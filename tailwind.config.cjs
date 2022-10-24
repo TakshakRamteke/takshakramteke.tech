7be038c051b6e93302046a76a044eeb828fa6a86
TakshakRamteke <takshakramteke0708@gmail.com>
2022-10-24 23:39:49 +0530

    FEAT : new animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -32,11 +32,33 @@ module.exports = {
             transform: "translateY(100%)",
           },
         },
+        moveleft: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(100%)",
+          },
+          100: {
+            opacity: "1.0",
+            transform: "translateX(-100%)",
+          },
+        },
+        moveright: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-100%)",
+          },
+          100: {
+            opacity: "1.0",
+            transform: "translateX(100%)",
+          },
+        },
       },
       animation: {
         fadein: "fadein 0.4s ease-in",
         moveup: "moveup 0.5s ease-in-out",
         movedown: "movedown 0.5s ease-in-out",
+        moveright: "moveright 0.2s ease-in-out",
+        moveleft: "moveleft 0.3s ease-in-out",
       },
     },
   },
