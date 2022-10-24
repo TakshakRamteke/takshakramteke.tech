c098f68d57a7403442e540cfaa486c85a62ac431
TakshakRamteke <takshakramteke0708@gmail.com>
2022-10-24 20:25:05 +0530

    FEAT : Added some Animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        fadein: {
+          "0%": { opacity: "0" },
+          "50%": { opacity: "0.5" },
+          "100%": { opacity: "1.0" },
+        },
+        moveup: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(100%)",
+          },
+          100: {
+            opacity: "1.0",
+            transform: "translateY(-100%)",
+          },
+        },
+        movedown: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-100%)",
+          },
+          100: {
+            opacity: "1.0",
+            transform: "translateY(100%)",
+          },
+        },
+      },
+      animation: {
+        fadein: "fadein 0.4s ease-in",
+        moveup: "moveup 0.5s ease-in-out",
+        movedown: "movedown 0.5s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+};
