4690246d656fa15f68f6957494565cd21f07ad57
TakshakRamteke <takshakramteke0708@gmail.com>
2022-11-01 20:51:09 +0530

    FEAT : cards for blogs

---
+++ src/pages/blogs/e2e-tests-made-easy.md
@@ -0,0 +1,9 @@
+---
+layout: ../../layouts/Layout.astro
+title: 'E2E tests made easy with cypress + cucumber '
+cover: '/assets/blogs/e2e-blog-cover.png'
+author: 'Takshak Ramteke'
+date: '20-Aug-2022'
+---
+
+# E2E tests made easy using cucumber + cypress
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,64 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        inter: ['Inter', 'sans-serif'],
+        ibm: ['IBM Plex Sans', 'sans-serif'],
       },
       keyframes: {
         fadein: {
-          "0%": { opacity: "0" },
-          "50%": { opacity: "0.5" },
-          "100%": { opacity: "1.0" },
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0.5' },
+          '100%': { opacity: '1.0' },
         },
         moveup: {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(100%)",
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(100%)',
           },
           100: {
-            opacity: "1.0",
-            transform: "translateY(-100%)",
+            opacity: '1.0',
+            transform: 'translateY(-100%)',
           },
         },
         movedown: {
-          "0%": {
-            opacity: "0",
-            transform: "translateY(-100%)",
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-100%)',
           },
           100: {
-            opacity: "1.0",
-            transform: "translateY(100%)",
+            opacity: '1.0',
+            transform: 'translateY(100%)',
           },
         },
         moveleft: {
-          "0%": {
-            opacity: "0",
-            transform: "translateX(100%)",
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(100%)',
           },
           100: {
-            opacity: "1.0",
-            transform: "translateX(-100%)",
+            opacity: '1.0',
+            transform: 'translateX(-100%)',
           },
         },
         moveright: {
-          "0%": {
-            opacity: "0",
-            transform: "translateX(-100%)",
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-100%)',
           },
           100: {
-            opacity: "1.0",
-            transform: "translateX(100%)",
+            opacity: '1.0',
+            transform: 'translateX(100%)',
           },
         },
       },
       animation: {
-        fadein: "fadein 0.4s ease-in",
-        moveup: "moveup 0.5s ease-in-out",
-        movedown: "movedown 0.5s ease-in-out",
-        moveright: "moveright 0.2s ease-in-out",
-        moveleft: "moveleft 0.3s ease-in-out",
+        fadein: 'fadein 0.4s ease-in',
+        moveup: 'moveup 0.5s ease-in-out',
+        movedown: 'movedown 0.5s ease-in-out',
+        moveright: 'moveright 0.2s ease-in-out',
+        moveleft: 'moveleft 0.3s ease-in-out',
       },
     },
   },
